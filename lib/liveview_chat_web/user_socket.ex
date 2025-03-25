676e0f013239abc3dcf478031a64ec5d7960aad0
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2025-03-25 15:35:39 +0100

    disable transport

--- lib/liveview_chat_web/user_socket.ex
+++ lib/liveview_chat_web/user_socket.ex
@@ -5,14 +5,14 @@ defmodule LiveviewChatWeb.UserSocket do
   channel "chat:*", LiveviewChatWeb.ChatChannel
 
   ## Transports (optional depending on your Phoenix version)
-  transport(:websocket, Phoenix.Transports.WebSocket)
+  #transport(:websocket, Phoenix.Transports.WebSocket)
 
   # Or use this if Phoenix >= 1.6
   # transport :websocket, Phoenix.Socket.Transport
 
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"user_id" => user_id}, socket, _connect_info) do
+    {:ok, assign(socket, :user_id, user_id)}
   end
 
-  def id(_socket), do: nil
+  def id(socket), do: "user_socket:#{socket.assigns.user_id}"
 end
