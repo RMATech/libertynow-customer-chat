d23aa83cb46affef480c670385696aca3ac8a90a
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2025-03-25 15:33:17 +0100

    publish subscribe message to dashboard

--- lib/liveview_chat_web/admin_chat_live.ex
+++ lib/liveview_chat_web/admin_chat_live.ex
@@ -2,14 +2,33 @@ defmodule LiveviewChatWeb.AdminChatLive do
   use LiveviewChatWeb, :live_view
   alias LiveviewChat.Message
 
+
+  # def mount(_params, _session, socket) do
+  #   if socket.assigns[:loggedin] do
+  #     Phoenix.PubSub.subscribe(LiveviewChat.PubSub, "chat:#{@store_id}")
+  #     messages = Message.list_messages()
+  #     grouped = Enum.group_by(messages, & &1.user_id)
+
+  #     {:ok,
+  #      assign(socket,
+  #        messages_by_user: grouped,
+  #        message_input: "",
+  #        current_user_id: nil
+  #      )}
+  #   else
+  #     {:ok, socket}
+  #   end
+  # end
   def mount(_params, _session, socket) do
     if socket.assigns[:loggedin] do
-      messages = Message.list_messages()
-      grouped = Enum.group_by(messages, & &1.user_id)
+      Phoenix.PubSub.subscribe(LiveviewChat.PubSub, "dashboard:store123")
+      messages = Message.list_messages() |> Enum.group_by(& &1.user_id)
 
+      IO.puts("Subscribed to dashboard:store123 ✅")
+  
       {:ok,
        assign(socket,
-         messages_by_user: grouped,
+         messages_by_user: messages,
          message_input: "",
          current_user_id: nil
        )}
@@ -17,7 +36,7 @@ defmodule LiveviewChatWeb.AdminChatLive do
       {:ok, socket}
     end
   end
-
+  
   def handle_event("select_user", %{"user_id" => user_id}, socket) do
     {:noreply, assign(socket, current_user_id: user_id)}
   end
@@ -44,4 +63,23 @@ defmodule LiveviewChatWeb.AdminChatLive do
        message_input: ""
      )}
   end
+  # def handle_info({:new_message, message}, socket) do
+  #   IO.inspect(message, label: "New incoming message")
+  #   # Update the grouped message list
+  #   updated_messages_by_user =
+  #     socket.assigns.messages_by_user
+  #     |> Map.update(message.user_id, [message], fn msgs -> [message | msgs] end)
+  
+  #   {:noreply, assign(socket, messages_by_user: updated_messages_by_user)}
+  # end
+  def handle_info({:message_created, message}, socket) do
+    IO.inspect(message, label: "New incoming message")
+  
+    updated_messages_by_user =
+      socket.assigns.messages_by_user
+      |> Map.update(message.user_id, [message], fn msgs -> [message | msgs] end)
+  
+    {:noreply, assign(socket, messages_by_user: updated_messages_by_user)}
+  end
+  
 end
